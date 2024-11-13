2f22ab729954f43c31ce291bd1d26c3dbac926ce
Soumyadipta Das <dassoumyadipta007@gmail.com>
2024-11-13 18:24:00 +0530

    check

--- app/Restaurant/dashboard/page.jsx
+++ app/Restaurant/dashboard/page.jsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
       <Navbar />
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <h1 className="text-3xl font-bold text-gray-900">Restaurant Dashboard</h1>
+          <h1 className="text-3xl font-bold text-gray-900">Bubun Dashboard</h1>
           <div className="space-x-4">
             <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
               $ Debtor Payments
