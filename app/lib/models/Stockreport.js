06994adbc5ad75369eb665f3af1fcb2d5b80566c
kingshere <barnikchakraborty24@gmail.com>
2024-11-20 01:28:26 +0530

    pushh

---
+++ app/lib/models/Stockreport.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+const Stockreport = new mongoose.Schema(
+    {
+        purchaseorderno:
+        {
+            type:String,
+            required: true,
+            unique: true,
+        },
+        purchasedate:
+        {
+            type:Date,
+            required:true,
+        },
+        Invoiceno:
+        {
+            type:String,
+            required:true,
+            unique:true,
+        },
+        quantity:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'InventoryList',
+            required:true,
+        },
+        unit:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'InventoryList',
+            required:true,            
+        },
+        rate:
+        {
+            type:Number,
+
+            required:true,
+        },
+        taxpercent:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'InventoryList',
+            required:true,
+        },
+        total:
+        {
+            type:Number,
+            required:true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);   
+
+const StockReport = mongoose.models.StockReport || mongoose.model("StockReport", Stockreport);
+
+export default StockReport; 
+
+
+
+
+
+
+
+
+
+
