0ddbb13c78be0ba42a1320c48156ec6b084b2d40
kingshere <barnikchakraborty24@gmail.com>
2024-11-28 01:55:38 +0530

    push2

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
     return (
         <div>
-            <footer className="bg-amber-800 text-white p-4 ">
+            <footer className="bg-[#1976d2] text-white p-4">
                 <div className="container mx-auto text-center">
                     © {new Date().getFullYear()}, Booking Master. All Rights Reserved.
                 </div>
             </footer>
         </div>
-    )
-}
+    );
+};
+
