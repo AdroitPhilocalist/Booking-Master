f715fc4db52a9896dc17a87ef83598323cbff6ba
Piyush Saha <sahapiyush5@gmail.com>
2025-01-03 07:49:08 +0530

    push

--- app/Restaurant/invoice/createinvoice/page.jsx
+++ app/Restaurant/invoice/createinvoice/page.jsx
@@ -541,6 +541,7 @@ const CreateInvoicePage = ({ onInvoiceCreate, existingInvoice, onCancel }) => {
                   <Typography variant="h6">Total Amount: ₹{formData.totalamt}</Typography>
                   <Typography variant="h6">IGST ₹{formData.gst.toFixed(2)} ({((formData.gst*100)/formData.totalamt)||0}%)</Typography>
                   <Typography variant="h7">CGST ₹{((formData.gst)/2).toFixed(2)} ({((formData.gst*100)/formData.totalamt)/2||0}%)</Typography>
+                  <br></br>
                   <Typography variant="h7">SGST ₹{((formData.gst)/2).toFixed(2)} ({((formData.gst*100)/formData.totalamt)/2||0}%)</Typography>
                   <Typography variant="h6">Payable Amount: ₹{formData.payableamt.toFixed(2)}</Typography>
                 </Box>
