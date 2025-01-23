066f54d570d8f569de52225efce76fa1c9be9c39
Piyush Saha <sahapiyush5@gmail.com>
2025-01-23 15:25:35 +0530

    included username attribute in Tables

--- app/lib/models/Tables.js
+++ app/lib/models/Tables.js
@@ -16,6 +16,10 @@ const TableSchema = new mongoose.Schema({
     enum: ['yes', 'no'],  // Restricts values to 'yes' or 'no'
     default: 'yes',       // Sets default value to 'yes'
   },
+  username: {  // New field
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.models.Table || mongoose.model('Table', TableSchema);
