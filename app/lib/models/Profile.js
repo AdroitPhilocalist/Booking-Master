063c3cb06fec7d771b44f6f0cdc990c42fb16bfa
Piyush Saha <sahapiyush5@gmail.com>
2025-01-23 17:46:11 +0530

    included username attribute in Profile

--- app/lib/models/Profile.js
+++ app/lib/models/Profile.js
@@ -56,6 +56,10 @@ const profileSchema = new mongoose.Schema({
     required: true,
   },
   Profile_Complete: { type: String, enum: ['yes', 'no'], default: 'no' },
+  username: {  // New field
+    type: String,
+    required: true,
+  },
 }, {
   timestamps: true, // Automatically manage createdAt and updatedAt fields
 });
