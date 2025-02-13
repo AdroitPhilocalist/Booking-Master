6ca5112f8b8d8da0fde3ad5389475eee34bfd173
Piyush Saha <sahapiyush5@gmail.com>
2025-02-13 21:31:26 +0530

    d

--- app/property/roomlist/page.jsx
+++ app/property/roomlist/page.jsx
@@ -158,7 +158,16 @@ export default function BookingManagement() {
       } else {
         const errorData = await res.json();
         console.error("Failed to create new room:", errorData.error);
-        toast.error("Failed to add new room!");
+        toast.error("Failed to add new room!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
       }
     } catch (error) {
       console.error("An error occurred while creating the room:", error);
