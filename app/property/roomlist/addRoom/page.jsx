a2165aeab9bea967251554a66a60566c9ec5ee6b
Piyush Saha <sahapiyush5@gmail.com>
2025-02-14 15:17:18 +0530

    rcb vs kkr

--- app/property/roomlist/addRoom/page.jsx
+++ app/property/roomlist/addRoom/page.jsx
@@ -8,7 +8,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, toast,Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function NewRoomForm() {
