518024be436643f8c6158bb1fc76dd4ded2100b8
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-02-28 19:41:48 +0530

    yiouo

--- app/Inventory/SalesReport/page.jsx
+++ app/Inventory/SalesReport/page.jsx
@@ -54,14 +54,24 @@ const SalesReportPage = () => {
     const fetchData = async () => {
       try {
         setIsLoading(true);
-        const token = getCookie('authToken'); // Get the token from cookies
-        if (!token) {
-          router.push('/'); // Redirect to login if no token is found
+        const token = getCookie('authToken');
+        const usertoken = getCookie("userAuthToken");
+        if (!token && !usertoken) {
+          router.push("/"); // Redirect to login if no token is found
           return;
         }
-        // Verify the token
-        const decoded = await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
-        const userId = decoded.payload.id;
+
+        let decoded, userId;
+        if (token) {
+          // Verify the authToken (legacy check)
+          decoded = await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
+          userId = decoded.payload.id;
+        }
+        if (usertoken) {
+          // Verify the userAuthToken
+          decoded = await jwtVerify(usertoken, new TextEncoder().encode(SECRET_KEY));
+          userId = decoded.payload.profileId; // Use userId from the new token structure
+        }
         // Fetch the profile by userId to get the username
         const profileResponse = await fetch(`/api/Profile/${userId}`);
         const profileData = await profileResponse.json();
@@ -509,8 +519,8 @@ const SalesReportPage = () => {
                       <TableCell sx={{ textAlign: "center" }}>{report.quantityAmount}</TableCell>
                       <TableCell sx={{ textAlign: "center" }}>{report.unit?.quantityUnit}</TableCell>
                       <TableCell sx={{ textAlign: "center" }}>{report.rate}</TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>{(report.taxpercent?.tax)/2}</TableCell>
-                      <TableCell sx={{ textAlign: "center" }}>{(report.taxpercent?.tax)/2}</TableCell>
+                      <TableCell sx={{ textAlign: "center" }}>{(report.taxpercent?.tax) / 2}</TableCell>
+                      <TableCell sx={{ textAlign: "center" }}>{(report.taxpercent?.tax) / 2}</TableCell>
                       <TableCell sx={{ textAlign: "center" }}>{report.total}</TableCell>
                     </TableRow>
                   ))
