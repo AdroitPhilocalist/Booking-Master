6ea989f839b71da46e1ed55e98bc5fe21fdb7977
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-02-28 19:26:28 +0530

    dfs

--- app/Inventory/Category/page.jsx
+++ app/Inventory/Category/page.jsx
@@ -181,14 +181,24 @@ export default function InventoryCategory() {
 
   const handleDeleteProduct = async (id) => {
     try {
-      const token = getCookie('authToken'); // Get the token from cookies
-      if (!token) {
-        router.push('/'); // Redirect to login if no token is found
+      const token = getCookie('authToken');
+      const usertoken = getCookie("userAuthToken");
+      if (!token && !usertoken) {
+        router.push("/"); // Redirect to login if no token is found
         return;
       }
-      // Verify the token
-      const decoded = await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
-      const userId = decoded.payload.id;
+
+      let decoded, userId;
+      if (token) {
+        // Verify the authToken (legacy check)
+        decoded = await jwtVerify(token, new TextEncoder().encode(SECRET_KEY));
+        userId = decoded.payload.id;
+      }
+      if (usertoken) {
+        // Verify the userAuthToken
+        decoded = await jwtVerify(usertoken, new TextEncoder().encode(SECRET_KEY));
+        userId = decoded.payload.profileId; // Use userId from the new token structure
+      }
       // Fetch the profile by userId to get the username
       const profileResponse = await fetch(`/api/Profile/${userId}`);
       const profileData = await profileResponse.json();
