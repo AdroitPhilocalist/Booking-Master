ba08915b79d3d2c69b6f509c5ec7c89b8429b24e
Soumyadipta Das <dassoumyadipta007@gmail.com>
2025-03-18 23:12:52 +0530

    Updates

--- app/lib/dbConnect.js
+++
@@ -1,3 +0,0 @@
-// Use environment variable for connection string with fallback for local development
-const connectSTR = process.env.MONGODB_URI || "mongodb+srv://devsolvotel:devsolvotel@cluster0.g5lid.mongodb.net/HotelDB?retryWrites=true&w=majority&appName=Cluster0";
-export default connectSTR;
